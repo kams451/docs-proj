8dc5c09dfafc38da94726e8e340d2505e8a3dd31
Kamrul Hassan <5591980+kams451@users.noreply.github.com>
2021-03-27 02:16:57 +0800

    Update esw-docs-global.js

--- esw-docs-global.js
+++ esw-docs-global.js
@@ -1,8 +1,9 @@
 (function($) {
     AJS.toInit(function() {  
-       alert("Boo!");; 
-       if (window.location.href.indexOf("Package+API+References") > -1) {
-        let embedContent = '<embed type="text/html" src="https://logistics-package-api.sandbox.eshopworld.com/swagger/index.html?urls.primaryName=V4" height="800">';   
+       alert("Boo!");
+       var href = window.location.href; 
+       if (href.indexOf("Package+API+References") > -1) {
+        var embedContent = '<embed type="text/html" src="https://logistics-package-api.sandbox.eshopworld.com/swagger/index.html?urls.primaryName=V4" height="800">';   
         $(".rw_adf_document").append(embedContent);
        }         
     });   
