2fc46e2c167cc538b0ae89d2f5a1d041f9fac3c5
Kamrul Hassan <5591980+kams451@users.noreply.github.com>
2021-03-27 01:43:12 +0800

    Add files via upload

---
+++ esw-docs-global.js
@@ -0,0 +1,9 @@
+(function($) {
+    /**
+     * When the document is ready
+     */
+    AJS.toInit(function() {  
+       $('#rw_wrapper').prepend("Hello world!");
+       alert("Hello world!");          
+    });   
+})(AJS.$);
