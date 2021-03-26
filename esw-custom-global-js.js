cfa698899f9487882c21a58c2a7597daf8232196
Kamrul Hassan <5591980+kams451@users.noreply.github.com>
2021-03-27 01:43:25 +0800

    Delete esw-custom-global-js.js

--- esw-custom-global-js.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    /**
-     * When the document is ready
-     */
-    AJS.toInit(function() {  
-       $('#rw_wrapper').prepend("Hello world!");
-       alert("Hello world!");          
-    });   
-})(AJS.$);
