09c32876d3c46c9de8a298b6fe3998361dea85e5
Kamrul Hassan <5591980+kams451@users.noreply.github.com>
2021-03-27 02:18:39 +0800

    Update esw-docs-global.js

--- esw-docs-global.js
+++ esw-docs-global.js
@@ -1,8 +1,8 @@
 (function($) {
     AJS.toInit(function() {  
-       alert("Boo!");
        var href = window.location.href; 
        if (href.indexOf("Package+API+References") > -1) {
+               alert("Boo!");   
         var embedContent = '<embed type="text/html" src="https://logistics-package-api.sandbox.eshopworld.com/swagger/index.html?urls.primaryName=V4" height="800">';   
         $(".rw_adf_document").append(embedContent);
        }         
