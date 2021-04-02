76c39dc347a558a843086ebd8dd1d31be29eec07
Kamrul Hassan <5591980+kams451@users.noreply.github.com>
2021-04-02 23:10:34 +0800

    Update esw-docs-global.js

--- esw-docs-global.js
+++ esw-docs-global.js
@@ -1,7 +1,11 @@
-//HLJS 
-setTimeout(function(){ 
-  hljs.highlightAll();
-  hljs.initHighlightingOnLoad();
-  hljs.initLineNumbersOnLoad();
-  console.log("123");
-}, 5000);
+$( "#rw_navigation_menu a" ).on( "click", function() {
+  //HLJS 
+  setTimeout(function(){ 
+    hljs.highlightAll();
+    hljs.initHighlightingOnLoad();
+    hljs.initLineNumbersOnLoad();
+    console.log("123");
+  }, 5000);
+  
+  
+});
