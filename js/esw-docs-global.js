5cce53960999601ded536f4112b4ab59b001d249
Kamrul Hassan <kamrul@webhelius.com>
2021-04-22 06:57:54 +0800

    Add Scroll Function

--- js/esw-docs-global.js
+++ js/esw-docs-global.js
@@ -7,13 +7,21 @@ setTimeout(function(){
   console.log("HL Init");
 }, 5000);
 
-window.onscroll = function() {navScrollFunction()};
-let navbar = document.getElementById("rw_header");
-let sticky = navbar.offsetTop;
-function navScrollFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+
+
+
+  (function ($) {
+    let dt = Date.now();
+    AJS.toInit(function() {
+      window.onscroll = function() {navScrollFunction()};
+      let navbar = document.getElementById("rw_header");
+      let sticky = navbar.offsetTop;
+      function navScrollFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
+    });
+  })(AJS.$);
