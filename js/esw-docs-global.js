e7c706bc4edced7e1b70066607d998d564a14110
Kamrul Hassan <kamrul@webhelius.com>
2021-04-22 07:05:09 +0800

    Add Scroll Function

--- js/esw-docs-global.js
+++ js/esw-docs-global.js
@@ -6,22 +6,3 @@ setTimeout(function(){
   hljs.initLineNumbersOnLoad();
   console.log("HL Init");
 }, 5000);
-
-
-
-
-  (function ($) {
-    let dt = Date.now();
-    AJS.toInit(function() {
-      window.onscroll = function() {navScrollFunction()};
-      let navbar = document.getElementById("rw_header");
-      let sticky = navbar.offsetTop;
-      function navScrollFunction() {
-        if (window.pageYOffset >= sticky) {
-          navbar.classList.add("sticky")
-        } else {
-          navbar.classList.remove("sticky");
-        }
-      }
-    });
-  })(AJS.$);
