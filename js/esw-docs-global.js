c40268dc221cc9b09149d4605c6f63df424858b2
Kamrul Hassan <kamrul@webhelius.com>
2021-04-23 20:26:14 +0800

    Adding Footer Border

--- js/esw-docs-global.js
+++ js/esw-docs-global.js
@@ -11,27 +11,9 @@ setTimeout(function(){
     AJS.toInit(function () {
       $( "#tryCheckout" ).click(function(e) {
         console.log("echo");
-        e.preventDefault();
-        let form = new FormData();
-        form.append("grant_type", "client_credentials");
-        form.append("scope", "checkout.preorder.api.all");
-        form.append("client_id", "gocasual.checkout.client");
-        form.append("client_secret", "?-:;U-!J!KQ_0ze_M_+DTJ}1wv]U}#W#");
-
-        let settings = {
-          "url": "https://security-sts.sandbox.eshopworld.com/connect/token",
-          "method": "POST",
-          "timeout": 0,
-          "processData": false,
-          "mimeType": "multipart/form-data",
-          "contentType": false,
-          "data": form
-        };
-
-        $.ajax(settings).done(function (response) {
-           console.log("Response",response);
-        });
-
       })
+    });
+    AJS.bind('rw-categories:menu-loaded', function(e, $el, categories){
+      console.log("Loaded");
     });
   })(AJS.$);
