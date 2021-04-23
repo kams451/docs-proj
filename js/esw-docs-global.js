33e0157fe638b07d459652da150ce4fbe4e6bd7f
Kamrul Hassan <kamrul@webhelius.com>
2021-04-23 20:48:51 +0800

    Adding Footer Border

--- js/esw-docs-global.js
+++ js/esw-docs-global.js
@@ -7,6 +7,12 @@ setTimeout(function(){
   //console.log("HL Init");
 }, 5000);
 
+setInterval(function(){
+  hljs.highlightAll();
+  //hljs.initHighlightingOnLoad();
+  hljs.initLineNumbersOnLoad();
+  }, 3000);
+
   (function ($) {
     AJS.toInit(function () {
       $( "#rw_navigation_menu ul li a" ).click(function(e) {
